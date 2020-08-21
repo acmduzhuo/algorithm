9ebb3b2161b19e6ddd7ab6c91301803a98e17cda
duzhuo <1638388435@qq.com>
2020-08-21 12:12:05 +0800

    add 数值输入处理

---
+++ Didi/test.js
@@ -0,0 +1,30 @@
+// var line = '5 5 11';
+// line = line.split(' ');
+// var a = Number(line[0]);
+// var b = Number(line[1]);
+// var c = parseInt(line[2]);
+// console.log("a = ", a);
+// console.log("b = ", b);
+// console.log("c = ", c);
+// if((c>=a+b) && (((c-a-b)&1) == 0 )){
+//     //print("Yes")
+//     console.log("Yes");
+// } else {
+//     //print("No")
+//     console.log("No")
+// }
+
+//小插曲
+var str = "1 2 3";
+//str = read_line();
+str = str.split(' ');
+
+var a = Number(str[0]);
+var b = Number(str[1]);
+var c = Number(str[2]);
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("c = ", c);
+
+var s = '1.23';
+console.log(Number(s));
