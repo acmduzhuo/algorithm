65fe464abbef89b551a1ac774b62aecd2b7ed89a
duzhuo <1638388435@qq.com>
2020-08-21 20:45:09 +0800

    add didi_test

--- Didi/test.js
+++ Didi/test.js
@@ -1,33 +1,56 @@
-// var line = '5 5 11';
-// line = line.split(' ');
-// var a = Number(line[0]);
-// var b = Number(line[1]);
-// var c = parseInt(line[2]);
-// console.log("a = ", a);
-// console.log("b = ", b);
-// console.log("c = ", c);
-// if((c>=a+b) && (((c-a-b)&1) == 0 )){
-//     //print("Yes")
-//     console.log("Yes");
-// } else {
-//     //print("No")
-//     console.log("No")
-// }
+var num = read_line();
+var gao = read_line();
+var yuan = read_line();
+num = Number(num);
 
-//小插曲
-// var str = "1 2 3";
-// //str = read_line();
-// str = str.split(' ');
-//
-// var a = Number(str[0]);
-// var b = Number(str[1]);
-// var c = Number(str[2]);
-// console.log("a = ", a);
-// console.log("b = ", b);
-// console.log("c = ", c);
-//
-// var s = '1.23';
-// console.log(Number(s));
 
-var customs = [[1,10], [1,3], [3,7], [5,9], [3,5]];
-console.log(customs.length);
+// var num = 10;
+// var gao = 'A B C D E F X H I J';
+// var yuan = 'A C I X E F J H D B';
+
+var arr = "";
+if(gao.length===1 && yuan.length===1){
+    arr+=1;
+    arr+=" ";
+    arr+=1;
+    //console.log(arr);
+    print(arr);
+}
+else {
+    var gao_c = gao.indexOf('X');
+    var yuan_c = yuan.indexOf('X');
+    var gao_q = gao.slice(0, gao_c);
+    var gao_h = gao.slice(gao_c + 1);
+    var yuan_q = yuan.slice(0, yuan_c);
+    var yuan_h = yuan.slice(yuan_c + 1);
+// console.log(gao_q);
+// console.log(gao_h);
+// console.log(yuan_q);
+// console.log(yuan_h);
+    var good = 0, bad = 0;
+    for (var i = 0; i < gao_q.length; i++) {
+        if (gao_q[i] !== ' ') {
+            for (var j = 0; j < yuan_q.length; j++) {
+                if (gao_q[i] === yuan_q[j]) {
+                    good++;
+                }
+            }
+        }
+    }
+    for (var i = 0; i < gao_h.length; i++) {
+        if (gao_h[i] !== ' ') {
+            for (var j = 0; j < yuan_h.length; j++) {
+                if (gao_h[i] === yuan_h[j]) {
+                    bad++;
+                }
+            }
+        }
+    }
+    good += 1;
+    bad = num - bad;
+    arr += good;
+    arr += " ";
+    arr += bad;
+    print(arr);
+    //console.log(arr);
+}
---
+++ 模板/字符串.js
@@ -0,0 +1,6 @@
+var arr = ['a', 'b', 'c', 'a'];
+console.log(arr.indexOf('b'));
+console.log(arr.lastIndexOf('a'));
+var str = 'abcda';
+console.log(str.indexOf('a', 1));
+console.log(str.lastIndexOf('a'));
