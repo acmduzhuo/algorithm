717abada3a30ce9d484221b7cba417cbc96ee0db
duzhuo <1638388435@qq.com>
2020-08-23 21:57:04 +0800

    add TX_test

--- Tencent/test.js
+++ Tencent/test.js
@@ -1,11 +1,56 @@
-var line = readline();
+// var str = "A B C D E F G H I J K";
+// str = str.replace(/\s*/g,"");
+// console.log(str);
+// console.log(str.charAt(1));
+// console.log(str[1]);
+// console.log(str.charCodeAt(1));//660
+// var str_new = str.concat("LM");
+// console.log(str_new);
+// console.log(str.valueOf());//ABCDEFGHIJK
+// console.log(str_new.valueOf());//ABCDEFGHIJKLM
+// console.log(str.toLowerCase());
 
+// var line = readline();
+//
+// var lineNum = parseInt(readline());
+// for(var i=0;i<lineNum;i++){
+//     var lines = readline();
+// }
+//
+// while(lines = readline()){
+//
+// }
 
-var lineNum = parseInt(readline());
-for(var i=0;i<lineNum;i++){
-    var lines = readline();
+var str = readline();
+var num = readline();
+
+// var str = '';
+// var num = '2';
+
+num = Number(num);
+if(num === 0 ){
+    console.log('...');
+} else {
+    var i = 0;
+    var sum = 0;
+    while (i !== str.length) {
+        //(str[i]>='0'&&str[i]<='9')||(str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')
+        if (str.charCodeAt(i) > 255) {
+            sum += 4;
+        } else {
+            sum += 2;
+        }
+        i++;
+        if (sum >= num) {
+            break;
+        }
+        //console.log("sum = ", sum);
+    }
+    var str_1 = str.substring(0, i);
+    if (i !== str.length) {
+        console.log(str_1.concat('...'));
+    } else {
+        console.log(str_1);
+    }
 }
 
-while(lines = readline()){
-    
-}
