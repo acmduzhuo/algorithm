d65746a2b5d1b3c0ea84e9f35cc9e0e3a8b0312f
duzhuo <1638388435@qq.com>
2020-09-03 21:00:05 +0800

    add Baidu_GG

---
+++ baidu/test.js
@@ -0,0 +1,22 @@
+// var str = "A B C D E F G H I J K";
+// str = str.replace(/\s*/g,"");
+// console.log(str);
+// console.log(str.charAt(1));
+// console.log(str[1]);
+// console.log(str.charCodeAt(1));//660
+// var str_new = str.concat("LM");
+// console.log(str_new);
+// console.log(str.valueOf());//ABCDEFGHIJK
+// console.log(str_new.valueOf());//ABCDEFGHIJKLM
+// console.log(str.toLowerCase());
+
+// var line = readline();
+//
+// var lineNum = parseInt(readline());
+// for(var i=0;i<lineNum;i++){
+//     var lines = readline();
+// }
+//
+// while(lines = readline()){
+//
+// }
