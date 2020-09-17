10fd10356c83e4497bf13ec42876ce280fe9ca96
duzhuo <1638388435@qq.com>
2020-09-17 21:35:12 +0800

    add JD_test

---
+++ Jingdong/test.js
@@ -0,0 +1,27 @@
+// var line = read_line();
+//var n = read_line();
+//var m = read_line();
+
+// var str = read_line();
+// str = str.split(' ');
+// n = str[0];
+// m = str[1];
+// n = 60;
+// m = 1;
+// n = Number(n);
+// m = Number(m);
+// m *= 100;
+// var sum = 0;
+// i = 0;
+// while(sum !== m){
+//     sum += n;
+//     for(var j=1;j<=(i+1);j++){
+//         var two = Math.pow(2, j);
+//         sum -= parseFloat(n/two);
+//     }
+//     i++;
+// }
+// console.log(i);
+
+console.log(parseFloat(1/2));
+
